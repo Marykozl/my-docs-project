0396be4f74f92572aaf3b33fd3ac52d91bd12093
Марина Козлюк <ist251_kmo@student.ztu.edu.ua>
2026-02-02 17:36:56 +0200

    Завдання 2.2

---
+++ source/repos/OOPLab2.2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPLab2._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введiть x:");
+            double x = double.Parse(Console.ReadLine());
+            Console.Write("Введiть y:");
+            double y = double.Parse(Console.ReadLine());
+            Console.Write("Введiть z:");
+            double z = double.Parse(Console.ReadLine());
+
+            double s = Math.Pow(x * x * x + y * y * y + z * z * z, 1.0 / 3.0) * Math.Cos(z) + Math.Abs(x - y);
+            Console.WriteLine($"s = {s}");
+        }
+    }
+}
